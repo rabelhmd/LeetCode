319c6c5f202fad64526831af94645513487f7b12
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-03 19:48:31 +0600

    Time: 23 ms (17.77%), Space: 7.3 MB (75.49%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     
-    void getFilter(string &s) {
-        while(s[0] == ' ') s.erase(0, 1);
-        while(s[s.size()-1] == ' ') s.pop_back();
-    }
-    
-    string getAns(vector <string> &v) {
-        string str = "";
-        for(int i = v.size()-1; i >= 0; i--) {
-            str += v[i];
-            str += (i > 0 ? " " : "");
-        }
-        return str;
-    }
-    
     string reverseWords(string s) {
-        getFilter(s);
         stringstream ss(s);
-        string w;
-        vector <string> v;
-        while(ss >> w) v.push_back(w);
-        return getAns(v);
+        string ans, w;
+        while(ss >> w) ans.insert(0, w + " ");
+        if(ans.size() > 0) ans.pop_back();
+        return ans;
     }
 };
