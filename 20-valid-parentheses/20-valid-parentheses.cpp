a59a4d72bee0d9085aebb4b9aedc76c0c925c292
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-04 19:32:58 +0600

    Time: 3 ms (44.84%), Space: 6.2 MB (96.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    
     bool isOpening(char c) {
         return (c == '(' || c == '{' || c == '[');
     }
     
-    bool inValid(char cur, char top) {
-        if(cur == ')' && top != '(') return true;
-        if(cur == '}' && top != '{') return true;
-        if(cur == ']' && top != '[') return true;
+    bool valid(char cur, char last) {
+        if(last == '(' && cur == ')') return true;
+        if(last == '{' && cur == '}') return true;
+        if(last == '[' && cur == ']') return true;
         return false;
     }
     
     bool isValid(string s) {
         stack <char> St;
-        for(auto c: s) {
-            if(isOpening(c)) {
-                St.push(c);
+        for(auto bracket: s) {
+            if(isOpening(bracket)) {
+                St.push(bracket);
                 continue;
             }
             if(St.size() <= 0) return false;
-            char top = St.top(); St.pop();
-            if(inValid(c, top)) return false;
+            char last = St.top(); St.pop();
+            if(!valid(bracket, last)) return false;
         }
         return St.empty();
     }
