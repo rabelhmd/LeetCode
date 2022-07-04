3a2bffc5241a58208ba10d3d117be3dc88ac6f05
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-04 19:42:57 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (74.48%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int sumBase(int n, int k) {
-        int sum = 0;
-        while(n) sum += n%k, n/=k;
-        return sum;
+        if(n == 0) return 0;
+        return n%k + sumBase(n/k, k);
     }
 };
