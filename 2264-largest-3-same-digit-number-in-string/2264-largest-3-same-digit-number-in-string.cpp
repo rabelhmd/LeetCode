547c54c48cecf2be8af08fe1657e7ddb7b2fc4b3
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-05 11:46:23 +0600

    Time: 7 ms (25.42%), Space: 6.5 MB (44.46%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     string largestGoodInteger(string num) {
         int len = num.size();
-        string ans = "";
+        char d = '$';
         for(int i = 0; i < len-2; i++) {
             if(num[i] == num[i+1] && num[i+1] == num[i+2]) {
-                if(ans == "") {
-                    ans += num[i];
-                    ans += num[i+1];
-                    ans += num[i+2];
+                if(d == '$') {
+                    d = num[i];
+                    continue;
                 }
-                if(num[i] > ans[0]) {
-                    ans = "";
-                    ans += num[i];
-                    ans += num[i+1];
-                    ans += num[i+2];
+                if(num[i] > d) {
+                    d = num[i];
                 }
-                //ans = max(ans, (num[i] + num[i+1] + num[i+2]));
             }
         }
+        if(d == '$') return "";
+        string ans = "";
+        ans += d;
+        ans += d;
+        ans += d;
         return ans;
     }
 };
