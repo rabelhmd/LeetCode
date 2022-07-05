b9865732735c0efbb09d667b519218effd05b445
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-05 08:43:44 +0600

    Time: 267 ms (52.56%), Space: 51.3 MB (14.39%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(nums.size() == 0) return 0;
-        
-        
         set <int> St;
         for(auto x: nums) St.insert(x);
         
         auto it = St.begin();
-        int start = *it, count = 1, ans = 1;
-        ++it;
+        int start = *it++, count = 1, ans = 1;
         for(; it != St.end(); ++it) {
             if(*it - start == 1) {
                 count += 1;
@@ -19,7 +16,6 @@ public:
             ans = max(ans, count);
             start = *it;
         }
-        
         return ans;
     }
 };
