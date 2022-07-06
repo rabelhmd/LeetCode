182bb80894e9ee00f58fb9d8bc858fafef270a00
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-06 11:49:46 +0600

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+// C++ program to rearrange an array in minimum 
+// maximum form 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // This function wants you to modify the given input
+    // array and no need to return anything
+    // arr: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    void rearrange(long long *arr, int n)  { 
+        vector <long long> v(n);
+        for(int i = 0; i < n; i++) v[i] = arr[i];
+        //sort(v.begin(), v.end());
+        for(int i = 0, j = n-1, id = 0; i <= j; i++, j--) {
+            arr[id++] = v[j];
+            arr[id++] = v[i];
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver program to test above function 
+int main() 
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        
+        //size of array
+        int n;
+        cin >> n;
+        
+        long long arr[n];
+        
+        //adding elements to the array
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        
+        //calling rearrange() function
+        ob.rearrange(arr, n);
+        
+        //printing the elements
+        for (int i = 0; i < n; i++) 
+		    cout << arr[i] << " ";
+		
+		cout << endl;
+    }
+	return 0; 
+} 
+  // } Driver Code Ends
