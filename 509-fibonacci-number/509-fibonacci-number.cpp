f72b4cfa95b0b2018901f78f93e1c62782e0b662
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-06 11:07:46 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int fib(int n) {
+        int f[35];
+        f[0] = 0, f[1] = 1;
+        for(int i = 2; i <= 30; i++) f[i] = f[i-1] + f[i-2];
+        return f[n];
+    }
+};
