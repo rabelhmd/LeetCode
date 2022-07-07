b6dfc83cfe3912d935bfad6547edbbe0b005ed0b
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-07 10:09:09 +0600

    Time: 4 ms (52.72%), Space: 9 MB (89.70%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& A, int m, vector<int>& B, int n) {
+    
         int cur = 0;
+        
         while(cur < n) A[m++]  = B[cur++];
+    
         sort(A.begin(), A.end());
     }
 };
