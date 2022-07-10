bb1b120b16e610d6c3860426bc702833845a8e96
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-10 13:26:25 +0600

    Time: 42 ms (35.55%), Space: 21.6 MB (9.83%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,32 +1,73 @@
 class Solution {
 public:
     
-    bool isValid(vector<vector<char>>& board, int x1, int y1, int x2, int y2) {
+    bool isValid(vector<char> &b) {
         int counter = 0;
         set <char> St;
-        for(int i = x1; i <= x2; i++) {
-            for(int j = y1; j <= y2; j++) {
-                if(board[i][j] != '.') {
-                    if(St.find(board[i][j]) != St.end()) return false;
-                    St.insert(board[i][j]);
-                    //counter += 1;
+        for(auto c: b) {
+            if(c != '.') {
+                if(St.find(c) != St.end()) return false;
+                St.insert(c);
+            }
+        }
+        return true;
+    }
+    
+    bool isValid2d(vector<vector<char>>& b) {
+        int counter = 0;
+        set <char> St;
+        for(auto r: b) {
+            for(auto c: r) {
+                if(c != '.') {
+                    if(St.find(c) != St.end()) return false;
+                    St.insert(c);
                 }
             }
         }
         return true;
     }
     
-    bool isValidSudoku(vector<vector<char>>& board) {
+    vector<vector<char>> genGrid(int iStart, int iEnd, int jStart, int jEnd, vector<vector<char>>& board) {
+        vector<vector<char>> grid;
+        for(int i = iStart; i < iEnd; i++) {
+            vector <char> v;
+            for(int j = jStart; j < jEnd; j++) v.push_back(board[i][j]);
+            grid.push_back(v);
+        }
+        return grid;
+    }
+    
+    // void out(vector<vector<char>> g) {
+    //     for(auto r: g) {
+    //         for(auto c: r) {
+    //             cout << c << " ";
+    //         }
+    //         cout << "\n";
+    //     }
+    // }
+    
+    bool isValidSudoku(vector<vector<char>> board) {
         
         for(int i = 0; i < 9; i++) {
-            if (!isValid(board, i, 0, i, 8)) return false;
-            if (!isValid(board, 0, i, 8, i)) return false;
+            if(isValid(board[i]) == false) return false;
         }
         
-        for (int i = 0; i < 3;i++)
-            for(int j = 0; j < 3;j++)
-                if (!isValid(board, i*3, j*3, i*3+2, j*3+2))
-                    return false;
+        for(int i = 0; i < 9; i++) {
+            vector<char> b(9);
+            for(int j = 0; j < 9; j++) b[j] = board[j][i];
+            if(isValid(b) == false) return false;
+        }
+        
+        vector<vector<char>> g;
+        
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                g = genGrid(i*3, i*3 + 3, j*3, j*3 + 3, board);
+                if(!isValid2d(g)) return false;
+            }
+        }
+        
+
         return true;
     }
 };
