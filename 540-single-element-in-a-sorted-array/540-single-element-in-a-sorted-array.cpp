ec53596dd70e9bf63aca9de31c5de4923740669e
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-10 09:31:53 +0600

    Time: 35 ms (69.44%), Space: 22.4 MB (54.98%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums[0];
-        for(int i = 1; i < nums.size(); i++) n ^= nums[i];
+        int n = nums[0], len = nums.size();
+        for(int i = 1; i < len; i++) n ^= nums[i];
         return n;
     }
 };
