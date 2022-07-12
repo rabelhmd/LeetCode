0e16641b0337978c7b85bcd9106afb46bd4a4fe2
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-12 21:56:30 +0600

    Time: 19 ms (67.69%), Space: 13.7 MB (43.92%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.cpp
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -20,7 +20,7 @@ public:
         
         int len = timePoints.size(), mn = INT_MAX;
         for(int i = 1; i < len; i++) mn = min(mn, mins[i] - mins[i-1]);
-        mn = min(mn, mins[0] - mins[len-1] + 1440);
+        mn = min(mn, 1440-(mins[len-1] - mins[0]));
 
         return mn;
     }
