fdbe6a639ca5952bf159094bdbee615f6b2ad262
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-12 17:10:43 +0600

    Time: 32 ms (9.56%), Space: 11.6 MB (11.35%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     
-    void getString(vector <int> & nums, vector <string>& v) {
+    void getString(vector <int> & nums, vector <string>& v, string& res) {
         for(auto n: nums) v.push_back(to_string(n));
         
         sort(v.begin(), v.end(), [](string &s1, string &s2) { 
             return s1 + s2 > s2 + s1; 
         });
+        
+        for(auto x: v) res += x;
     }
     
     string largestNumber(vector<int>& nums) {
         vector <string> v;
-        getString(nums, v);
-        
         string res, ans;
-        for(auto x: v) res += x;
+        getString(nums, v, res);
         
         int id = 0, len = res.size();
-        while(id < res.size() && res[id] == '0') id += 1;
+        while(id < len && res[id] == '0') id += 1;
         
         for(; id < len; id++) ans += res[id];
         return ans.size() == 0 ? to_string(nums[0]) : ans;
