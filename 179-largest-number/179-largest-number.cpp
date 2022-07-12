7043f4ea485bbc3ab614fc983344f9f2a68bfdaa
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-12 17:07:21 +0600

    Time: 25 ms (17.62%), Space: 11.7 MB (8.76%) - LeetHub

---
+++ 179-largest-number/179-largest-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    void getString(vector <int> & nums, vector <string>& v) {
+        for(auto n: nums) v.push_back(to_string(n));
+    }
+    
+    string largestNumber(vector<int>& nums) {
+        vector <string> v;
+        getString(nums, v);
+        
+        sort(v.begin(), v.end(), [](string &s1, string &s2) { 
+            return s1 + s2 > s2 + s1; 
+        });
+        
+        string res, ans;
+        for(auto x: v) res += x;
+        
+        int id = 0, len = res.size();
+        while(id < res.size() && res[id] == '0') id += 1;
+        
+        for(; id < len; id++) ans += res[id];
+        return ans.size() == 0 ? to_string(nums[0]) : ans;
+    }
+};
