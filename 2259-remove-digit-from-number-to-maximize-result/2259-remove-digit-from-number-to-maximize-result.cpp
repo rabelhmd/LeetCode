e06c014d3cbb691694fd0306a45603ea28f7067d
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-13 14:29:47 +0600

    Time: 3 ms (58.73%), Space: 6.9 MB (40.84%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -7,7 +7,7 @@ public:
         for(int i = 0; i < len; i++) {
             if(number[i] == digit) {
                 cur = number.substr(0, i) + number.substr(i+1);
-                if(cur > str) str = cur;
+                str = max(cur, str);
             }
         }
         
