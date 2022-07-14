e2bbbd0cea73441394a6621b8fa70b876f957c0b
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-14 12:46:37 +0600

    Time: 54 ms (29.91%), Space: 25.9 MB (79.05%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -14,12 +14,10 @@ public:
     
     TreeNode* build(vector<int>& preo, vector<int>& ino, int& id, int left, int right) {
         if(left > right) return NULL;
-        
         int pv = left;
         while(ino[pv] != preo[id]) pv += 1;
 
         id += 1;
-        
         TreeNode* cur = new TreeNode(ino[pv]);
         cur -> left = build(preo, ino, id, left, pv - 1);
         cur -> right = build(preo, ino, id, pv + 1, right);
@@ -28,7 +26,6 @@ public:
     
     TreeNode* buildTree(vector<int>& preo, vector<int>& ino) {
         int id = 0, left = 0, right = ino.size()-1;
-        
         return build(preo, ino, id, left, right);
     }
 };
