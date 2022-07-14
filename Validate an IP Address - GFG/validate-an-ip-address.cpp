712836b319a288ecf665e162decaf7bda6800f3f
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-14 20:42:30 +0600

    Added solution - LeetHub

--- Validate an IP Address - GFG/validate-an-ip-address.cpp
+++ Validate an IP Address - GFG/validate-an-ip-address.cpp
@@ -13,9 +13,10 @@ class Solution {
             
             int id = 0, len = s.size();
             
-            if(s[0] == '.') return false;
+            if(isdigit(s[0]) == false) return false;
             
             for(int i = 1; i < len; i++) {
+                if(s[i] != '.' && isdigit(s[i]) == false) return false;
                 if(s[i] == '.' && s[i-1] == '.') return false;
                 if(s[i] == '.') id += 1;
             }
@@ -26,28 +27,23 @@ class Solution {
             id = 0;
             
             while(id < len && s[id] != '.') {
-                if(isdigit(s[id]) == false) return false;
                 a += s[id++];
             }
             id += 1;
             while(id < len && s[id] != '.') {
-                if(isdigit(s[id]) == false) return false;
                 b += s[id++];
             }
             id += 1;
             while(id < len && s[id] != '.') {
-                if(isdigit(s[id]) == false) return false;
                 c += s[id++];
             }
             id += 1;
             while(id < len && s[id] != '.') {
-                if(isdigit(s[id]) == false) return false;
                 d += s[id++];
             }
             id += 1;
             
             if(a == "" || b == "" || c == "" || d == "") return 0;
-            
             
             if(a.size() > 1 && a[0] =='0' || b.size() > 1 && b[0] =='0' || c.size() > 1 && c[0] =='0' || d.size() > 1 && d[0] =='0') return 0;
             
