26373755fafe2740670ff24f0d25367dfbf2adae
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-17 23:09:12 +0600

    Time: 12 ms (19.09%), Space: 15.4 MB (6.28%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    //Input: strs = ["flower","flow","flight"]
-    //Output: "fl"
-    string longestCommonPrefix(vector<string>& strs) {
-        int overallSize = strs.size();
-        if (overallSize == 0) return "";
-        int first = 0, firstSize = strs[first].size();
-        for(int i = 0; i < firstSize; i++) {
-            char ch = strs[first][i];            
-            for(int j = 1; j < overallSize; j++) {
-                string cur = strs[j];
-                int Len = cur.size();
-                if(i == Len || cur[i] != ch)
-                    return strs[first].substr(0, i);
+    string longestCommonPrefix(vector<string>& v) {
+        if (v.size() == 0) return "";
+        int len = v[0].size();
+        for(int i = 0; i < len; i++) {
+            char ch = v[0][i];            
+            for(int j = 1; j < v.size(); j++) {
+                string cur = v[j];
+                if(i == cur.size() || cur[i] != ch)
+                    return v[0].substr(0, i);
             }
         }
-        return strs[first];
+        return v[0];
     }
 };
