16a45024e59292c49b3b1ad5923f353d59ef9cdc
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-21 15:48:12 +0600

    Time: 42 ms (67.66%), Space: 12.8 MB (15.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     
-    int dp[2505][2505];
-    
-    int LIS(vector<int>& v, int cur, int last) {
-        if(cur >= v.size()) return 0;
+    int LIS(vector<int>& v) {
         
-        int &ret = dp[cur][last];
-        if(ret != -1) return ret;
-        ret = max(ret, LIS(v, cur + 1, last));
-        if(v[cur] > v[last]) {
-            ret = max(ret, 1 + LIS(v, cur + 1, cur));
+        multiset < int > St;
+        multiset < int > :: iterator it;
+        
+        for(auto x: v) {
+            St.insert(x);
+            it = St.lower_bound(x);
+            it ++;
+            if(it != St.end()) St.erase(it);
         }
-        return ret;
+        return St.size();
     }
     
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        vector <int> v;
-        v.push_back(INT_MIN);
-        for(auto x: nums) v.push_back(x);
-        return LIS(v, 1, 0);
+        return LIS(nums);
     }
 };
