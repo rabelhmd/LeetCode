fecbfacb9986910d4f83aba82e7005161bf4042c
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-21 15:41:00 +0600

    Time: 2682 ms (5.00%), Space: 38.4 MB (12.81%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int dp[2505][2505];
+    
+    int LIS(vector<int>& v, int cur, int last) {
+        if(cur >= v.size()) return 0;
+        
+        int &ret = dp[cur][last];
+        if(ret != -1) return ret;
+        ret=0;
+        if(v[cur] > v[last]) {
+            ret = 1 + LIS(v, cur + 1, cur);
+        }
+        
+        ret = max(ret, LIS(v, cur + 1, last));
+        return ret;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        vector <int> v;
+        v.push_back(INT_MIN);
+        for(auto x: nums) v.push_back(x);
+        return LIS(v, 1, 0);
+    }
+};
