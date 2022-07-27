f803d52aa8072dc8a03c68e0fd9af0e95b6ebf3e
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-27 19:45:31 +0600

    Time: 47 ms (70.11%), Space: 14.3 MB (48.53%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int len, maxLen = 0;
+    string res;
+    
+    void palindromeLen(string &s, int x, int y) {
+        if(y >= len) return;
+        while(x >= 0 && y < len && s[x] == s[y]) x--, y++;
+        int dif = y - x - 1;
+        if(dif > maxLen) {
+            maxLen = dif;
+            res = s.substr(x + 1, dif);
+        }
+    }
+    
+    string longestPalindrome(string s) {
+        len = s.size();
+        maxLen = 0;
+        for(int i = 0; i < len; i++) {
+             palindromeLen(s, i, i); // odd len palindrome
+             palindromeLen(s, i, i+1); // even len
+        }
+        return res;
+    }
+};
