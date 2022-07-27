2a3bcea0773d5d50b18e9e54cdb1b8452f6ea8e5
Rabel Ahmed <rabelhmd@gmail.com>
2022-07-27 15:01:45 +0600

    Time: 63 ms (48.34%), Space: 89.8 MB (30.80%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for(auto x: nums2) nums1.push_back(x);
+        sort(nums1.begin(), nums1.end());
+        int len = nums1.size();
+        if(len == 1) return nums1[0];
+        if(len % 2 == 0) {
+            len /= 2;
+            double x = nums1[len-1] + nums1[len];
+            return x/2.0;
+        } 
+        double x = nums1[len/2];
+        return x;
+    }
+};
