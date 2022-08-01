e365294db90255c2cdbf67e3063911867c8dc110
Rabel Ahmed <rabelhmd@gmail.com>
2022-08-01 08:48:06 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     
     int dp[101][101];
-    
-    int track(int x, int y, int& row, int& col) {
+    int rec(int x, int y, int& row, int& col) {
         if(x > row || y > col) return 0;
-        
         if(x == row && y == col) return 1;
-        
         int &ret = dp[x][y];
         if(~ret) return ret;
-        
-        ret = track(x+1, y, row, col) + track(x, y+1, row, col);
+        ret = rec(x + 1, y, row, col) + rec(x, y + 1, row, col);
         return ret;
     }
     
-    int uniquePaths(int r, int c) {
+    int uniquePaths(int m, int n) {
         memset(dp, -1, sizeof(dp));
-        r--, c--;
-        return track(0, 0, r, c);
+        m -= 1, n -= 1;
+        return rec(0, 0, m, n);
     }
 };
