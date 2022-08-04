34c2183f0a87c590685726722da3e6d92c4d608d
Rabel Ahmed <rabelhmd@gmail.com>
2022-08-04 11:22:42 +0600

    Time: 4 ms (23.97%), Space: 5.9 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        while(p%2 == 0 && q % 2 == 0) {
+            p /= 2, q /= 2;
+        }
+        if(p & 1 && q & 1) return 1;
+        if(p % 2 == 0) return 2;
+        return 0;
+    }
+};
