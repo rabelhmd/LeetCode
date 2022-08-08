0c88bc03f1aab78e130fe728f58003fd0433f46b
Rabel Ahmed <rabelhmd@gmail.com>
2022-08-08 09:09:06 +0600

    Time: 19 ms (76.77%), Space: 12.9 MB (19.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     
-    int LIS(vector<int>& v) {
-        
-        multiset < int > St;
-        multiset < int > :: iterator it;
-        
-        for(auto x: v) {
-            St.insert(x);
-            it = St.lower_bound(x);
-            it ++;
-            if(it != St.end()) {
-                St.erase(it);
-            }
+    int cuteLIS(vector<int>& nums) {
+        multiset <int> St;
+        multiset <int> :: iterator it;
+        for(auto &c: nums) {
+            St.insert(c);
+            it = St.lower_bound(c); it++;
+            if(it != St.end()) St.erase(it);
         }
         return St.size();
     }
     
     int lengthOfLIS(vector<int>& nums) {
-        return LIS(nums);
+        return cuteLIS(nums);
     }
 };
