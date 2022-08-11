7230876f8bc01d0a18c253d74b4ae53bc4631db4
Rabel Ahmed <rabelhmd@gmail.com>
2022-08-11 10:21:10 +0600

    Time: 12 ms (87.21%), Space: 21.6 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,13 +12,14 @@
 class Solution {
 public:
     
-    TreeNode* prev;
+    TreeNode* prev = NULL;
     
     bool isValidBST(TreeNode* root) {
         if(!root) return true;
-        if(!isValidBST(root->left)) return false;
-        if(prev != NULL && prev->val >= root->val) return false;
+        if(!isValidBST(root -> left)) return false;
+        
+        if(prev != NULL && prev -> val >= root -> val) return false;
         prev = root;
-        return isValidBST(root->right);
+        return isValidBST(root -> right);
     }
 };
