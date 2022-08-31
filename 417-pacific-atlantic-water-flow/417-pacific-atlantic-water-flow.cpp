3267de6405fbd7098de6ebd2ea1250f62a2d9acb
Rabel Ahmed <rabelhmd@gmail.com>
2022-08-31 21:25:02 +0600

    Time: 635 ms (5.61%), Space: 16.9 MB (98.50%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    int row, col;
+    bool flag[201][201];
+    
+    pair<int, int> dfs(int x, int y, vector<vector<int>>& H) {
+        if(x < 0 || y < 0 || x >= row || y >= col) {
+            return {x < 0 || y < 0, x >= row || y >= col};
+        }
+        
+        flag[x][y] = true;
+        pair<bool, bool> pr = {0, 0};
+        for(int i = 0; i < 4; i++) {
+            int r = x + dx[i];
+            int c = y + dy[i];
+
+            if(r < 0 || c < 0 || r >= row || c >= col || (H[r][c] <= H[x][y] && flag[r][c] ==false)) {
+                pair<int, int> cur = dfs(r, c, H);
+                pr.first |= cur.first;
+                pr.second |= cur.second;
+            }
+        }
+        
+        return {pr.first, pr.second};
+    }
+    
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        row = heights.size();
+        col = heights[0].size();
+        vector<vector<int>> res;
+        
+        for(int i = 0; i < row; i++) {
+            
+            for(int j = 0; j < col; j++) {
+                pair<int, int> cur = dfs(i, j, heights);
+                
+                if(cur.first && cur.second) {
+                    res.push_back({i, j});
+                }
+                
+                memset(flag, false, sizeof(flag));
+            }
+        }
+        
+        return res;
+    }
+};
