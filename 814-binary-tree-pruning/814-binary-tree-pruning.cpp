90993fe35a9931dfcf44257172b0bf26cde85e40
Rabel Ahmed <rabelhmd@gmail.com>
2022-09-06 08:56:51 +0600

    Time: 4 ms (49.90%), Space: 8.9 MB (21.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -11,14 +11,11 @@
  */
 class Solution {
 public:
-    
     TreeNode* pruneTree(TreeNode* root) {
         if(!root) return root;
-        root -> left = pruneTree(root-> left);
-        root -> right = pruneTree(root-> right);
-        if(!root->left && !root->right && !root->val == 1) {
-            return NULL;
-        }
+        root -> left = pruneTree(root -> left);
+        root -> right = pruneTree(root -> right);
+        if(!root -> left && !root -> right && !root -> val == 1) return NULL;
         return root;
     }
 };
