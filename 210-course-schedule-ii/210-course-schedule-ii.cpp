0a3e135f8ff655e69685871324eb610571e75b7a
Rabel Ahmed <rabelhmd@gmail.com>
2022-10-06 22:32:45 +0600

    Time: 47 ms (37.81%), Space: 13.3 MB (86.51%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(n, vector <int> ());
-        vector <int> degree(n, 0);
-        for(auto &v: pre) {
-            adj[v[1]].push_back(v[0]);
-            degree[v[0]] += 1;
+        vector <vector <int>> adj(n, vector <int> ());
+        vector <int> degree(n , 0);
+        for(auto &x: pre) {
+            adj[x[1]].push_back(x[0]);
+            degree[x[0]] += 1;
         }
         
         queue <int> q;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i += 1) {
             if(degree[i] == 0) q.push(i);
         }
         
@@ -19,9 +19,7 @@ public:
             ret.push_back(u);
             for(auto &v: adj[u]) {
                 degree[v] -= 1;
-                if(degree[v] == 0) {
-                    q.push(v);
-                }
+                if(degree[v] == 0) q.push(v);
             }
         }
         if(ret.size() != n) return {};
