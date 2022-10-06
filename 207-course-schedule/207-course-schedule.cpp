85fa38696c6002f0c5942a26ba51ca5204bd2354
Rabel Ahmed <rabelhmd@gmail.com>
2022-10-06 19:36:27 +0600

    Time: 36 ms (61.54%), Space: 13.4 MB (83.58%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     
-    bool topSort(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(n, vector <int>());
-        vector<int> degree(n, 0);
-        for(auto &x: pre) {
-            adj[x[1]].push_back(x[0]);
-            degree[x[0]] += 1;
+    bool canFinish(int n, vector<vector<int>>& info) {
+        vector <vector<int>> adj(n, vector<int>());
+        vector <int> degree(n, 0);
+        for(auto &x: info) {
+            int niteChai = x[1];
+            int lagbe = x[0];
+            adj[niteChai].push_back(lagbe);
+            degree[lagbe] += 1;
         }
-    
+        
         queue<int> q;
         for(int i = 0; i < n; i++) {
             if(degree[i] == 0) q.push(i);
         }
         
         while(!q.empty()) {
-            int u = q.front(); q.pop();
-            n--;
-            for(auto v: adj[u]) {
-                degree[v]--;
+            int u = q.front(); q.pop(); n -= 1;
+            for(auto &v: adj[u]) {
+                degree[v] -= 1;
                 if(degree[v] == 0) q.push(v);
             }
         }
         return n == 0;
-    }
-    
-    bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        return topSort(numCourses, pre);
     }
 };
