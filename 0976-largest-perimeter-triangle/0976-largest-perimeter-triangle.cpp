ffcfacc9a60418b692a7444a50fc1f97826e0d0d
Rabel Ahmed <rabelhmd@gmail.com>
2022-10-12 08:42:30 +0600

    Time: 100 ms (9.98%), Space: 22 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -3,9 +3,8 @@ public:
     int largestPerimeter(vector<int>& nums) {
         sort(nums.rbegin(), nums.rend());
         for(int i = 0; i < nums.size()-2; i++) {
-            int a = nums[i], b = nums[i+1], c = nums[i+2];
-            if (a<b+c && b<c+a && c<a+b) {
-                return a+b+c;
+            if (nums[i]<nums[i+1]+nums[i+2] && nums[i+1]<nums[i+2]+nums[i] && nums[i+2]<nums[i]+nums[i+1]) {
+                return nums[i+0]+nums[i+1]+nums[i+2];
             }
         }
         return 0;
