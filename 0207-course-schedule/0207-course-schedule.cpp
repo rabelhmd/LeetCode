f4b6623ab7af796e248fc82296c8514a74b056a6
Rabel Ahmed <rabelhmd@gmail.com>
2022-10-18 20:08:21 +0600

    Time: 32 ms (74.04%), Space: 13 MB (98.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
+    
     int visit[5005];
-    vector<int> graph[100005];
+    vector <int> graph[5005];
     
     bool canFinish(int n, vector<vector<int>>& info) {
         for(auto &pr: info) {
-            int x = pr[0];
-            int y = pr[1];
+            int x = pr[0], y = pr[1];
             graph[y].push_back(x);
             visit[x] += 1;
         }
         
-        queue <int> q;
+        queue<int> q;
         for(int i = 0; i < n; i++) if(visit[i] == 0) q.push(i);
         
-        while(!q.empty()) {
-            int x = q.front(); q.pop(); n -= 1;
-            
+        while(q.empty() == false) {
+            int x = q.front(); q.pop();
+            n -= 1;
             for(auto &v: graph[x]) {
                 visit[v] -= 1;
                 if(visit[v] == 0) q.push(v);
