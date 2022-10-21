3de8e3d0816754ca22eb68beea1222c11757bdd5
Rabel Ahmed <rabelhmd@gmail.com>
2022-10-21 09:42:52 +0600

    Time: 2 ms (62.14%), Space: 6.4 MB (23.61%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int id = 0;
-        for(auto &x: t) id += s[id] == x;
+        for(auto &x: t) if(s[id] == x) id += 1;
         return id == s.size();
     }
 };
