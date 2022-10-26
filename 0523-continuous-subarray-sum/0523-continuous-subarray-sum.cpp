fa7c4560a1b5ec0f20b978873805c7d1b5d8a65b
Rabel Ahmed <rabelhmd@gmail.com>
2022-10-26 08:50:09 +0600

    Time: 484 ms (47.25%), Space: 113.8 MB (46.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,7 +3,7 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int len = nums.size();
         int sum = 0, temp = 0;
-        map <int, int> Mp;
+        unordered_map <int, int> Mp;
         for(int i = 0; i < len; i++) {
             sum = (sum + nums[i]) % k;
             if(Mp.find(sum) != Mp.end()) return true;
