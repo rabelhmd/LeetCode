ea6ec10af350d57dd636c7fa765244f1dd6ffb87
Rabel Ahmed <rabelhmd@gmail.com>
2022-11-23 17:21:42 +0600

    Time: 42 ms (63.50%), Space: 22 MB (5.67%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    bool invalid1D(vector<char> &b) {
+        unordered_set <char> St;
+        for(auto c: b)
+            if(c != '.') {
+                if(St.find(c) != St.end()) return true;
+                St.insert(c);
+            }
+        return false;
+    }
+    
+    bool invalid2D(vector<vector<char>>& b) {
+        unordered_set <char> St;
+        for(auto r: b)
+            for(auto c: r) 
+                if(c != '.') {
+                    if(St.find(c) != St.end()) return true;
+                    St.insert(c);
+                }
+        return false;
+    }
+    
+    vector<vector<char>> genGrid(int iStart, int iEnd, int jStart, int jEnd, vector<vector<char>>& board) {
+        vector<vector<char>> grid(3);
+        for(int i = iStart, pos = 0; i < iEnd; i++, pos += 1) {
+            for(int j = jStart; j < jEnd; j++) grid[pos].push_back(board[i][j]);
+        }
+        return grid;
+    }
+    
+    bool isValidSudoku(vector<vector<char>> board) {
+        
+        for(int i = 0; i < 9; i++) if(invalid1D(board[i])) return false;
+        
+        for(int i = 0; i < 9; i++) {
+            vector<char> b(9);
+            for(int j = 0; j < 9; j++) b[j] = board[j][i];
+            if(invalid1D(b)) return false;
+        }
+        
+        vector<vector<char>> g;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                g = genGrid(i*3, i*3 + 3, j*3, j*3 + 3, board);
+                if(invalid2D(g)) return false;
+            }
+        }
+        return true;
+    }
+};
