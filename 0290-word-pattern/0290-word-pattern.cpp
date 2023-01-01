33de02a06899f80f67dcfcfb72f15bf8a86e02d6
Rabel Ahmed <rabelhmd@gmail.com>
2023-01-01 09:44:00 +0600

    Time: 4 ms (32.33%), Space: 6.5 MB (19.87%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -4,7 +4,8 @@ public:
         map <char, char> Mp1;
         map <string, char> Mp2;
         istringstream ss(s);
-        string w; int id = 0; char hash = 'a';
+        string w; int id = 0; 
+        char hash = 'a';
         while(ss >> w) {
             char ch = pattern[id++];
             if(Mp1[ch] != Mp2[w]) return false;
