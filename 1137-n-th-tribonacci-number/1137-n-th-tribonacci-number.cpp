29331eb4713afc1d0f9346e140ef0148707e1cf2
Rabel Ahmed <rabelhmd@gmail.com>
2023-01-30 09:00:42 +0600

    Time: 0 ms (100.00%), Space: 6.1 MB (37.24%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        vector<int> t(n+5, 0);
+        t[1] = 1, t[2] = 1;
+        for(int i = 3; i <= n; i++) t[i] = t[i-1] + t[i-2] + t[i-3];
+        return t[n];
+    }
+};
