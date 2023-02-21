548dc5cd253d37f19bdf2de3dbf8675e6e147dab
Rabel Ahmed <rabelhmd@gmail.com>
2023-02-21 11:58:58 +0600

    Time: 178 ms (66.89%), Space: 96.5 MB (74.30%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int len = nums.size(), last = nums[len-1];
+        if(nums[0] <= last) {
+            for(int i = 1; i < len; i++) {
+                if(nums[i-1] > nums[i]) return false;
+            }
+            return true;
+        }
+        for(int i = 1; i < len; i++) {
+            if(nums[i-1] < nums[i]) return false;
+        }
+        return true;
+    }
+};
