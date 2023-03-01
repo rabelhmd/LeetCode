908ff1aea52d97115569c2a87f2ba4b84f226844
Rabel Ahmed <rabelhmd@gmail.com>
2023-03-01 13:48:21 +0600

    Time: 206 ms (58.10%), Space: 68.6 MB (35.06%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
+        priority_queue <int> pq;
+        for(auto &x: nums) pq.push(-x);
+        vector<int> ret;
+        while(pq.empty() == false) {
+            ret.push_back(-pq.top());
+            pq.pop();
+        }
+        return ret;
     }
 };
