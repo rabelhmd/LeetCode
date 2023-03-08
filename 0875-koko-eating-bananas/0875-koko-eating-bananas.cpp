898c8c19a369278daf33587ddd469cbe5c03020e
Rabel Ahmed <rabelhmd@gmail.com>
2023-03-08 10:08:24 +0600

    Time: 59 ms (44.88%), Space: 18.9 MB (45.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    
-   bool canEatAll(const vector<int>& piles, int h, int k) {
-       int hours = 0;
+    bool canEatAll(const vector<int>& piles, int h, int k) {
+        int hours = 0;
         for (int bananas : piles) {
             hours += (bananas + k - 1) / k;
             if (hours > h) {
@@ -13,14 +12,16 @@ public:
     }
     
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = 1e9;
+        sort(piles.rbegin(), piles.rend());
+        int n = piles.size();
+        int left = 1, right = piles[0];
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (canEatAll(piles, h, mid)) {
                 right = mid;
-                continue;
+            } else {
+                left = mid + 1;
             }
-            left = mid + 1;
         }
         return left;
     }
