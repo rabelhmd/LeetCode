473eb9600c712488bbaca72921dcddd12bdf8392
Rabel Ahmed <rabelhmd@gmail.com>
2023-03-20 10:21:08 +0600

    Time: 22 ms (40.04%), Space: 20.2 MB (92.39%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flower, int n) {
-        if(!n) return true;
+        flower.insert(flower.begin(), 0); flower.push_back(0);
+        
         int len = flower.size();
-        for(int i = 0; i < len; i++) {
-            if(!flower[i] && (!i || !flower[i-1]) && (i == len-1 || !flower[i+1])) {
-                flower[i] = 1, n--;
+        for(int i = 1; i < len-1; i++) {
+            if(flower[i-1] + flower[i] + flower[i+1] == 0) {
+                i += 1, n -= 1;
                 if(!n) return true;
             }
         }
-        return false;
+        return n <= 0;
     }
 };
