a5be22cdc9652aa49ec4d5bec83529507fe758bd
Rabel Ahmed <rabelhmd@gmail.com>
2023-03-22 11:49:20 +0600

    Time: 431 ms (92.77%), Space: 125.5 MB (72.00%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int bfs(int& n, vector<vector<int>>& roads) {
-        vector <vector<int>> adj[n+1];
-        for(auto &a: roads) {
-            int& x = a[0], &y = a[1], &z = a[2];
-            adj[x].push_back({y, z}), adj[y].push_back({x, z});
-        }
+    int bfs(int& n, vector<vector<pair<int, int>>>& adj) {
+        vector<bool> visit(n + 1);
+        queue<int> q; int ret = INT_MAX;
         
-        vector<bool> vis(n+1, false);
-        queue <vector<int>> q;
-        vis[1] = true; q.push({1, INT_MAX});
-        
-        int ret = INT_MAX;
+        q.push(1); visit[1] = true;
         while(!q.empty()) {
-            int cur = q.front()[0]; q.pop();
-            for(auto &a: adj[cur]) {
-                int &x = a[0], &y = a[1];
-                ret = min(ret, y);
-                if(!vis[x]) {
-                    q.push({x, y});
-                    vis[x] = true;
+            int node = q.front(); q.pop();
+            
+            for(auto& edge: adj[node]) {
+                ret = min(ret, edge.second);
+                if(!visit[edge.first]) {
+                    visit[edge.first] = true;
+                    q.push(edge.first);
                 }
             }
         }
@@ -27,6 +20,12 @@ public:
     }
     
     int minScore(int n, vector<vector<int>>& roads) {
-        return bfs(n, roads);
+        vector<vector<pair<int, int>>> adj(n+1);
+        for(auto& road: roads) {
+            int&x = road[0], &y = road[1], &z = road[2];
+            adj[x].push_back({y, z});
+            adj[y].push_back({x, z});
+        }
+        return bfs(n, adj);
     }
 };
