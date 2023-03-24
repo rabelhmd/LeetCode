18e1adc009be17d80f933519c7c6b1630e042d36
Rabel Ahmed <rabelhmd@gmail.com>
2023-03-24 15:06:32 +0600

    Time: 359 ms (83.53%), Space: 105.5 MB (83.53%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -3,27 +3,24 @@ public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<int>> adj(n), back(n);
         for(auto &a: connections) {
-            adj[a[0]].push_back(a[1]);
-            back[a[1]].push_back(a[0]);
+            int& x = a[0], &y = a[1];
+            adj[x].push_back(y);
+            back[y].push_back(x);
         }
         
         queue <int> q; q.push(0);
         vector <bool> visit(n, false);
-        int ret = 0;
-        while(!q.empty()) {
-            int cur = q.front(); q.pop();
-            visit[cur] = true;
+        int ret = 0; while(!q.empty()) {
+            int cur = q.front(); q.pop(); visit[cur] = true;
             for(auto &node: adj[cur]) {
                 if(!visit[node]) {
                     visit[node] = true;
-                    ret += 1;
-                    q.push(node);
+                    ret += 1; q.push(node);
                 }
             }
             for(auto &node: back[cur]) {
                 if(!visit[node]) {
-                    visit[node] = true;
-                    q.push(node);
+                    visit[node] = true; q.push(node);
                 }
             }
         }
