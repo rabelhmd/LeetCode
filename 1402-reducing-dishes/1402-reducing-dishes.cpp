bf2d2986c45dc2c762a48492ea407a50043911b6
Rabel Ahmed <rabelhmd@gmail.com>
2023-03-29 15:57:03 +0600

    Time: 11 ms (76.11%), Space: 8.4 MB (69.31%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.rbegin(), satisfaction.rend());
+        
+        vector<int> sum = satisfaction;
+        int len = sum.size();
+        for(int i = 1; i < len; i++) {
+            sum[i] += sum[i-1];
+            cout << sum[i] << endl;
+        }
+        
+        int ret = 0, cur = 0;
+        for(int i = 0; i < len; i++) {
+            cur += sum[i];
+            ret = max(ret, cur);
+        }
+        return ret;
+    }
+};
