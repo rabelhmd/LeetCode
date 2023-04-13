88775d03942cd14acae761f776b24fee48cda299
Rabel Ahmed <rabelhmd@gmail.com>
2023-04-13 12:19:41 +0600

    Time: 13 ms (33.03%), Space: 15.5 MB (24.28%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack <int> St;
+        int id = 0;
+        for(auto x: pushed) {
+            St.push(x);
+            while(!St.empty() and St.top() == popped[id]) {
+                id += 1; St.pop();
+            }
+        }
+        return St.empty();
+    }
+};
