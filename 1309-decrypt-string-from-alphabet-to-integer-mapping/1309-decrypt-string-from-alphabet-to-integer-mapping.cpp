186ddeeadf2df5f238c53507bd8508548b71b032
Rabel Ahmed <rabelhmd@gmail.com>
2023-04-27 11:47:36 +0600

    Time: 0 ms (100.00%), Space: 6.4 MB (16.49%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    unordered_map <string, char> Mp;
+    void doMap() {
+        for(int i = 1, a = 'a'; i <= 9; i++) {
+            Mp[to_string(i)] = a;
+            a += 1;
+        }
+        
+        for(int i = 10, a = 'j'; i <= 26; i++) {
+            Mp[to_string(i) + "#"] = a;
+            a += 1;
+        }
+    }
+    
+    string freqAlphabets(string s) {
+        doMap();
+        string ret;
+        for(int i = 0; i < s.size(); i++) {
+            if (i < s.size() - 2 && s[i + 2] == '#') {
+                string str = s.substr(i, 3); i += 2;
+                ret += Mp[str];
+            } else {
+                ret += Mp[to_string(s[i] - '0')];
+            }
+        }
+        return ret;
+    }
+};
