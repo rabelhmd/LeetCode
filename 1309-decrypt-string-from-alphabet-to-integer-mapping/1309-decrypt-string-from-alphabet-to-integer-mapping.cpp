3c683579da0243df53237ad587ad1a793b206525
Rabel Ahmed <rabelhmd@gmail.com>
2023-04-27 11:50:43 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (48.11%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -3,11 +3,7 @@ public:
     unordered_map <string, char> Mp;
     void doMap() {
         for(int i = 1, a = 'a'; i <= 26; i++, a++) {
-            if(i < 10) {
-                Mp[to_string(i)] = a;
-            } else {
-                Mp[to_string(i) + "#"] = a;
-            }
+            Mp[to_string(i)] = a;
         }
     }
     
@@ -16,7 +12,7 @@ public:
         string ret;
         for(int i = 0; i < s.size(); i++) {
             if (i < s.size() - 2 && s[i + 2] == '#') {
-                string str = s.substr(i, 3); i += 2;
+                string str = s.substr(i, 2); i += 2;
                 ret += Mp[str];
             } else {
                 ret += Mp[to_string(s[i] - '0')];
