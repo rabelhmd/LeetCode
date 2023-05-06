07ef3b6b783339cf30f35848b59a8031f605b9a1
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-06 12:08:15 +0600

    Time: 156 ms (29.73%), Space: 50 MB (37.87%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mod = 1000000007;
+    int numSubseq(vector<int>& nums, int target) {
+        int l = 0, len = nums.size(), r = len-1, ret = 0;
+        vector <int> pows(len+1, 1);
+        for(int i=1; i <= len; i++) {
+            pows[i] = (pows[i-1] * 2) % mod;
+        }
+        
+        sort(nums.begin(), nums.end());
+        while(l <= r) {
+            if(nums[l] + nums[r] > target) r -= 1;
+            else {
+                ret = (ret + pows[r-l++]) % mod;
+            }
+        }
+        return ret;
+    }
+};
