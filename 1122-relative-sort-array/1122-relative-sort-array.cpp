2dd2108d7325167aad0bf0efaeb5aa354ec9b8ac
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-14 11:57:18 +0600

    Time: 6 ms (38.06%), Space: 7.6 MB (94.66%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -5,14 +5,14 @@ public:
         for(int i = 0; i <= 1000; i++) dp[i] = 0;
         for(auto &x: arr1) dp[x] += 1;
         
-        vector<int> ret;
+        int pos = 0;
         for(auto &x: arr2) {
-            while(dp[x]-- > 0) ret.push_back(x);
+            while(dp[x]-- > 0) arr1[pos++] = x;
         }
         
         for(int i = 0; i <= 1000; i++) {
-            while(dp[i]-- > 0) ret.push_back(i);
+            while(dp[i]-- > 0) arr1[pos++] = i;
         }
-        return ret;
+        return arr1;
     }
 };
