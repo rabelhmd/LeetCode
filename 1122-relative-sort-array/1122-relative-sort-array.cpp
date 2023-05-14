ab4597045c6df68921b3e22306f9eac7a8f23045
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-14 11:51:27 +0600

    Time: 0 ms (100.00%), Space: 7.6 MB (83.77%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map <int, int> Mp;
-        for(auto &x: arr1) Mp[x] += 1;
+        int dp[10001];
+        for(int i = 0; i <= 1000; i++) dp[i] = 0;
+        for(auto &x: arr1) dp[x] += 1;
         
         vector<int> ret;
         for(auto &x: arr2) {
-            while(Mp[x]-- > 0) ret.push_back(x);
+            while(dp[x]-- > 0) ret.push_back(x);
         }
         
-        for(auto it: Mp) {
-            while(it.second-- > 0) ret.push_back(it.first);
+        for(int i = 0; i <= 1000; i++) {
+            while(dp[i]-- > 0) ret.push_back(i);
         }
-        
         return ret;
     }
 };
