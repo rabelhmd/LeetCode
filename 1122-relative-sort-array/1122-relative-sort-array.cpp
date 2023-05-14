4ab3091d080846ae33b74ec94f349b04af8ffc98
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-14 11:59:05 +0600

    Time: 3 ms (74.08%), Space: 7.6 MB (83.77%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -4,6 +4,7 @@ public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         for(auto &x: arr1) dp[x] += 1;
         int pos = 0;
+        
         for(auto &x: arr2) {
             while(dp[x]-- > 0) arr1[pos++] = x;
         }
@@ -11,6 +12,7 @@ public:
         for(int i = 0; i <= 1000; i++) {
             while(dp[i]-- > 0) arr1[pos++] = i;
         }
+        
         return arr1;
     }
 };
