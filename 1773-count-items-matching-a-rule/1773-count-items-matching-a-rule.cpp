07d63ce1e342ea9c0523f84ad02951e1d8c49f8e
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-14 16:42:30 +0600

    Time: 98 ms (5.45%), Space: 30.9 MB (36.82%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int f(int pos, string &target, vector<vector<string>>& items) {
-        int ret = 0;
-        for(auto &item: items) {
-            if(target == item[pos]) ret += 1;
-        }
-        return ret;
-    }
-    
     int getPos(char ch) {
         if(ch == 't') return 0;
         else if(ch == 'c') return 1;
         return 2;
     }
     
-    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        return f(getPos(ruleKey[0]), ruleValue, items);
+    int countMatches(vector<vector<string>>& items, string key, string target) {
+        int ret = 0, pos = getPos(key[0]);
+        for(auto &item: items) {
+            if(target == item[pos]) ret += 1;
+        }
+        return ret;
     }
 };
