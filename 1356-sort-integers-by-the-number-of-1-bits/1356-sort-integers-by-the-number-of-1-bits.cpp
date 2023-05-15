af1d6f7e6beec777bd8bac9eece7a8424e9b5715
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-15 15:56:43 +0600

    Time: 16 ms (25.64%), Space: 9.9 MB (97.51%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     static bool cmp(const int& a, const int& b) { 
-        int x = bitset<32>(a).count();
-        int y = bitset<32>(b).count();
+        int x = __builtin_popcount(a);
+        int y = __builtin_popcount(b);
         if(x != y)
             return x < y;
         return a < b;
