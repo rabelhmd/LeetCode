e0b0e87d881fd130a712041fcbda1c534aa5ccc0
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-17 00:14:45 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (52.51%) - LeetHub

--- 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    bool isspecial(char ch) {
-        string sp = "!@#$%^&*()-+";
-        return sp.find(ch) != string::npos;
-    }
     
     bool isStrong(string& p) {
         int len = p.size();
-        bool has8 = (len >= 8);
-        if(!has8) return false;
+        if(len < 8) return false;
         
         bool lo = false, up = false, d = false, sp = false;
         for(int i = 0; i < len; i++) {
-            if(!lo and islower(p[i])) lo = true;
-            if(!up and isupper(p[i])) up = true;
-            if(!d and isdigit(p[i])) d = true;
-            if(!sp and isspecial(p[i])) sp = true;
+            if(islower(p[i])) lo = true;
+            else if(isupper(p[i])) up = true;
+            else if(isdigit(p[i])) d = true;
+            else sp = true;
             if(i + 1 < len and p[i] == p[i+1]) return false;
         }
         // cout << lo << " " << up << " " << d << " " << sp << " " << adj << endl;
