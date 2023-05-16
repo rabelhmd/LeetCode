a907586f4ee49cfe65c1b7fd162b030e40d8c570
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-16 22:32:07 +0600

    Time: 23 ms (61.25%), Space: 11.2 MB (99.13%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        
+        int mx = -1, mn = 1e5+1;
+        for(int i = 0; i < m; i++) {
+            int mn = INT_MAX;
+            for(int j = 0; j < n; j++) mn = min(mn, mat[i][j]);
+            mx = max(mx, mn);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            int mx = INT_MIN;
+            for(int j = 0; j < m; j++) mx = max(mx, mat[j][i]);
+            mn = min(mn, mx);
+        }
+        
+        if(mx != mn) return {};
+        return {mx};
+    }
+};
