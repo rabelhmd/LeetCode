efbc88e3167b033c4c0ca30a7877e07e5e154eac
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-18 15:26:46 +0600

    Time: 0 ms (100.00%), Space: 7.8 MB (31.68%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
+    void faster() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
     bool isOnlyDigit(string &str) {
         for(auto &c: str) if(!isdigit(c)) return false;
         return true;
     }
     
     int maximumValue(vector<string>& strs) {
+        faster();
         int ret = 0;
         for(auto &str: strs) {
             int x;
