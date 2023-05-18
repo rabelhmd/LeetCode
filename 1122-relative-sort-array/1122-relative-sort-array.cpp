434945e1dd5e4c16b42c5bdfcc2e8933a2a3e249
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-18 15:25:12 +0600

    Time: 0 ms (100.00%), Space: 7.8 MB (62.49%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+    void faster() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
     int dp[1001], pos = 0;
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        faster();
         for(auto &x: arr1) dp[x] += 1;
         
-        for(auto &x: arr2) {
-            while(dp[x]-- > 0) arr1[pos++] = x;
-        }
+        for(auto &x: arr2) while(dp[x]-- > 0) arr1[pos++] = x;
         
-        for(int i = 0; i <= 1000; i++) {
-            while(dp[i]-- > 0) arr1[pos++] = i;
-        }
+        for(int i = 0; i <= 1000; i++) while(dp[i]-- > 0) arr1[pos++] = i;
         
         return arr1;
     }
