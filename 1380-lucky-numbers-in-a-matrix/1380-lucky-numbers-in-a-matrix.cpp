9cfd6850d3d18f79b6737f3dc59f4f92f73fe8c9
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-18 15:19:21 +0600

    Time: 20 ms (82.54%), Space: 11.4 MB (73.30%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,3 +1,9 @@
+int init = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& mat) {
