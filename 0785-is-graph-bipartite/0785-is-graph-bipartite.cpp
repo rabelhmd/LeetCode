7c834088085dcf0b326f0ad5c5f29b21b40bd350
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-19 11:38:43 +0600

    Time: 59 ms (6.12%), Space: 15.5 MB (14.65%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void faster() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr); cout.tie(nullptr);
+    }
+    
+    bool isBip(int i, vector<vector<int>>& graph, int& n) {
+        vector<int> colors(n, -1);
+        queue<int> q; q.push(i); colors[i] = 0;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            for (int& e: graph[cur]) {
+                if (colors[e] == -1) {
+                    colors[e] = 1 - colors[cur];
+                    q.push(e);
+                    continue;
+                }
+                
+                if (colors[e] == colors[cur]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        faster();
+        int n = graph.size();
+        for(int i = 0; i < n; i++) {
+            if(!isBip(i, graph, n)) return false;
+        }
+        return true;
+    }
+};
