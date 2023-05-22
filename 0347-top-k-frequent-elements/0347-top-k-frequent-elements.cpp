34179b343f8e29981a46d4e00dd373d0d8e33f69
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-22 13:29:18 +0600

    Time: 20 ms (43.48%), Space: 13.5 MB (97.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -20,11 +20,10 @@ public:
                 count += 1;
                 continue;
             }
-            Mp.push_back({nums[i-1], count});
+            Mp.push_back(make_pair(nums[i-1], count));
             count = 1;
         }
-        
-        Mp.push_back({nums[len-1], count});
+        Mp.push_back(make_pair(nums[len-1], count));
         sort(Mp.begin(), Mp.end(), cmp);
         
         vector <int> ret; for(int i = 0; i < k; i++) {
