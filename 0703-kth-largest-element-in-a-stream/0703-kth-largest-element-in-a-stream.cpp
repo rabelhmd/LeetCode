bd64c2be1f2b475608000097e5745d57a87ced31
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-23 11:27:23 +0600

    Time: 35 ms (68.84%), Space: 19.9 MB (60.44%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,22 +1,24 @@
 class KthLargest {
 public:
-    multiset <int> St;
+    priority_queue<int> pq;
     int k;
     void faster() {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr); cout.tie(nullptr);
     }
+    
     KthLargest(int k, vector<int>& nums) {
         faster();
-        this->k = k;
-        for(auto &x: nums) St.insert(x);
+        this-> k = k;
+        for(auto &x: nums) {
+            add(x);
+        }
     }
     
     int add(int val) {
-        St.insert(val);
-        auto it = St.begin();
-        for(int i = 0; i < St.size() - k; i++) it ++;
-        return *it;
+        pq.push(-val);
+        if(pq.size() > k) pq.pop();
+        return -pq.top();
     }
 };
 
