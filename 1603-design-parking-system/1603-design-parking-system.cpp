65ebb8dfd82d7089debfbb1553822f810d43963b
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-29 09:33:26 +0600

    Time: 78 ms (5.94%), Space: 33 MB (88.19%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,23 +1,14 @@
 class ParkingSystem {
 public:
-    int big, medium, small;
+    unordered_map<int, int> Mp;
     ParkingSystem(int big, int medium, int small) {
-        this -> big = big, this -> medium = medium, this -> small = small;
+        Mp[1] = big;
+        Mp[2] = medium;
+        Mp[3] = small;
     }
     
     bool addCar(int type) {
-        if(type == 1) {
-            big -= 1;
-            return (big >= 0) ? true : false;
-        }
-        if(type == 2) {
-            medium -= 1;
-            return (medium >= 0) ? true : false;
-        }
-        if(type == 3) {
-            small -= 1;
-            return (small >= 0) ? true : false;
-        }
+        if(Mp[type]-- > 0) return true;
         return false;
     }
 };
