1ea500154f1d530bfbedaac188936d52373bd899
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-29 16:48:57 +0600

    Time: 18 ms (6.25%), Space: 9 MB (43.75%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     string removeTrailingZeros(string num) {
-        int len = num.size() - 1;
-        while(num[len] == '0') {
-            len--;
-        }
-        return num.substr(0, len+1);
+        return num.substr(0, num.find_last_not_of('0') + 1);
     }
 };
