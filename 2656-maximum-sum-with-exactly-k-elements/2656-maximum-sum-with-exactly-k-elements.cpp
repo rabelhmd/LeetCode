062497a517704230120d205fc9f8bdce2defeff3
Rabel Ahmed <rabelhmd@gmail.com>
2023-05-31 17:04:10 +0600

    Time: 53 ms (47.04%), Space: 70.7 MB (29.21%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        int ret = 0, mx = *max_element(nums.begin(), nums.end());
-        while(k--> 0) {
-            ret += mx;
-            mx  += 1;
-        }
-        return ret;
+        int mx = *max_element(nums.begin(), nums.end());
+        return k * (k-1) / 2 + (k * mx);
     }
 };
