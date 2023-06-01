07bebfa6aedd81e60831f18833d77b16fbca0e32
Rabel Ahmed <rabelhmd@gmail.com>
2023-06-01 08:36:54 +0600

    Time: 75 ms (64.86%), Space: 19.2 MB (93.42%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,27 +3,24 @@ public:
     
     vector<int> dx = {-1,-1,-1,0,0,1,1,1};
     vector<int> dy = {-1,0,1,-1,1,-1,0,1};
-    bool isValidPoint(int x, int y, int n, int m) {
-        return x >= 0 && x <= n && y >= 0 && y <= m;
+    
+    bool isValid(int x, int y, int n, int m) {
+        return x >= 0 and x <= n and y >= 0 and y <= m;
     }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size()-1, m = grid[0].size()-1;
-        if (grid[0][0] || grid[n][m]) {
-            return -1;
-        }
+        int n = grid.size()-1, m = n;
+        if(grid[0][0] || grid[n][m]) return -1;
         
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        
-        while (!q.empty() && !grid[n][m]) {
+        queue <pair<int, int>> q;
+        q.push({0, 0}); grid[0][0] = 1;
+        while(!q.empty() and !grid[n][m]) {
             pair <int, int> pr = q.front(); q.pop();
             int a = pr.first, b = pr.second;
-            for (int i = 0; i < 8; i++) {
+            for(int i = 0; i < 8; i++) {
                 int x = a + dx[i];
                 int y = b + dy[i];
-                if (isValidPoint(x, y, n, m) && !grid[x][y]) {
+                if(isValid(x, y, n, m) and !grid[x][y]) {
                     grid[x][y] = grid[a][b] + 1;
                     q.push({x, y});
                 }
