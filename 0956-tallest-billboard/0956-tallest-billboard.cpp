bd9c8d582c6f92256c1b1f7cc83ce18ecad01605
Rabel Ahmed <rabelhmd@gmail.com>
2023-06-24 12:07:53 +0600

    Time: 97 ms (64.76%), Space: 8.7 MB (78.85%) - LeetHub

---
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -0,0 +1,22 @@
+const int offset = 5000;
+class Solution {
+public:
+    int dp[21][2 * offset + 1];
+    int solve(int pos, int diff, vector<int>& rods, int n) {
+        if (pos == n) {
+            if (diff == 0) return 0;
+            return INT_MIN;
+        }
+        int &ret = dp[pos][diff + offset];
+        if (ret != -1) return ret;
+        int a = solve(pos + 1 , diff, rods, n);
+        int b = rods[pos] + solve(pos + 1 , diff + rods[pos], rods, n);
+        int c = solve(pos + 1 , diff - rods[pos], rods, n);
+        return ret = max({a, b, c});
+    }
+    
+    int tallestBillboard(vector<int>& rods) {
+        memset(dp , -1 , sizeof(dp));
+        return max(0, solve(0, 0, rods, rods.size()));
+    }
+};
