ab5a42e17357a3de7ed88fa5c2bbd820d1c5ddd1
Rabel Ahmed <rabelhmd@gmail.com>
2023-07-03 10:33:56 +0600

    Time: 9 ms (15.93%), Space: 7 MB (52.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool buddyStrings(string s, string g) {
-        int lens = s.size(), leng = g.size();
-        if(lens != leng) return false;
-        vector <int> dif;
-        set <char> St;
+        int lens = s.size(), leng = g.size(); if(lens != leng) return false;
+        
+        vector <int> dif; set <char> St;
         for(int i = 0; i < lens; i++) {
             if(s[i] != g[i]) dif.push_back(i);
             St.insert(s[i]);
         }
+        
         if(s == g and St.size() < lens) return true;
         return dif.size() == 2 and s[dif[0]] == g[dif[1]] and s[dif[1]] == g[dif[0]];
     }
