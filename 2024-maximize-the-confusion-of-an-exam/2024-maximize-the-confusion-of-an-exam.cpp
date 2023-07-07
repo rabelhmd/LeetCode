7347c576ef7e590757b4dbcb1dadbde299692eba
Rabel Ahmed <rabelhmd@gmail.com>
2023-07-07 12:16:13 +0600

    Time: 23 ms (88.45%), Space: 9.9 MB (95.61%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int f(string& str, int& size, char ch, int& k) {
-        int count = 0, ret = 0, left = 0;
-        for(int i = 0; i < size; i++) {
-            if(str[i] == ch) count += 1;
-            while(count > k) {
-                if(str[left] == ch) count -= 1;
-                left++;
-            }
-            ret = max(ret, i - left + 1);
+    int f(string& str, int& k) {
+        char tr = 'T', fa = 'F';
+        int tc = 0, fc = 0, tl = 0, fl = 0, ret = 0, len = str.size();
+        for(int i = 0; i < len; i++) {
+            if(str[i] == tr) tc += 1;
+            if(str[i] == fa) fc += 1;
+            while(tc > k) if(str[tl++] == tr) tc -= 1;
+            while(fc > k) if(str[fl++] == fa) fc -= 1;
+            ret = max(ret, i - tl + 1);
+            ret = max(ret, i - fl + 1);
         }
         return ret;
     }
     
     int maxConsecutiveAnswers(string str, int k) {
-        int len = str.size();
-        return max(f(str, len, 'T', k), f(str, len, 'F', k));
+        return f(str, k);
     }
 };
