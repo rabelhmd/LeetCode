06003d8af76720bd39923af31a9c4ebd15b5afaa
Rabel Ahmed <rabelhmd@gmail.com>
2023-07-07 10:10:46 +0600

    Time: 32 ms (43.88%), Space: 10 MB (95.61%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int f(string& str, int& size, char ch, int& k) {
+        int count = 0, ret = 0, left = 0;
+        for(int i = 0; i < size; i++) {
+            if(str[i] == ch) count += 1;
+            while(count > k) {
+                if(str[left]== ch)count -= 1;
+                left++;
+            }
+            ret = max(ret, i - left + 1);
+        }
+        return ret;
+    }
+    
+    int maxConsecutiveAnswers(string str, int k) {
+        int len = str.size();
+        return max(f(str, len, 'T', k), f(str, len, 'F', k));
+    }
+};
