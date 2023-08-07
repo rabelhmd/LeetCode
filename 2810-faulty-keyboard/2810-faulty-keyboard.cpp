194a57b53e426c84ba4137177093513f1a9466a2
Rabel Ahmed <rabelhmd@gmail.com>
2023-08-07 06:07:28 +0600

    Time: 0 ms (100.00%), Space: 6.6 MB (100.00%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -3,12 +3,12 @@ public:
     
     string finalString(string s) {
         string ret;
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == 'i') {
+        for(auto& x: s) {
+            if(x == 'i') {
                 reverse(ret.begin(), ret.end());
                 continue;
             }
-            ret += s[i];
+            ret += x;
         }
         return ret;
     }
