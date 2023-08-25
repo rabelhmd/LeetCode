78d511d390a8cfbbab479f821768f6762e3a85b2
Rabel Ahmed <rabelhmd@gmail.com>
2023-08-25 09:47:19 +0600

    Time: 2 ms (81.15%), Space: 6.3 MB (90.66%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int len1 = s1.size();
+        int len2 = s2.size();
+        int len3 = s3.size();
+        if(len1 + len2 != len3) return false;
+        
+        bool dp[len1+1][len2+1];
+        char ch;
+        for(int i = 0; i < len1 + 1; i++) {
+            for(int j = 0; j < len2 + 1; j++) {
+                bool &ret = dp[i][j];
+                if(!i && !j) {
+                    ret = true;
+                    continue;
+                }
+                ch = s3[i+j-1];
+                if(!i) ret = (dp[i][j-1] && ch == s2[j-1]);
+                else if(!j) ret = (dp[i-1][j] && ch == s1[i-1]);
+                else ret = (dp[i-1][j] && ch == s1[i-1]) || (dp[i][j-1] && ch == s2[j-1]);
+            }
+        }
+        return dp[len1][len2];
+    }
+};
