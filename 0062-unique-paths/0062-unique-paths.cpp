3897f152431f73b62b3c2e00944eee89ed21f8c0
Rabel Ahmed <rabelhmd@gmail.com>
2023-09-03 19:58:09 +0600

    Time: 0 ms (100.00%), Space: 6 MB (78.55%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[101][101];
+    int rec(int x, int y, int& row, int& col) {
+        if(x > row || y > col) return 0;
+        if(x == row && y == col) return 1;
+        int& ret = dp[x][y];
+        if(~ret) return ret;
+        ret = rec(x + 1, y, row, col) + rec(x, y + 1, row, col);
+        return ret;
+    }
+    
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        m -= 1, n -= 1;
+        return rec(0, 0, m, n);
+    }
+};
