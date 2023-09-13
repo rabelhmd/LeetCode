abec2c12eee9541dea300790bb4b30ec4cb4db93
Rabel Ahmed <rabelhmd@gmail.com>
2023-09-13 19:31:11 +0600

    Time: 17 ms (62.30%), Space: 17.7 MB (47.60%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int prefixSum[2*10004];
+    int suffixSum[2*10004];
+    int ret(int len) {
+        int ans = 0;
+        for(int i = 0; i < len; i++) ans += max(prefixSum[i], suffixSum[i]);
+        return ans;
+    }
+    
+    int candy(vector<int>& ratings) {
+        int len = ratings.size();
+        if(len == 1) return 1;
+        
+        for(int i = 0; i < len; i++) prefixSum[i] = 1;
+        for(int i = 0; i < len; i++) suffixSum[i] = 1;
+        
+        for(int i = 1; i < len; i++) {
+            if(ratings[i] > ratings[i-1]) {
+                prefixSum[i] = prefixSum[i-1] + 1;
+            }
+        }
+        
+        for(int i = len-2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1]) {
+                suffixSum[i] = suffixSum[i+1] + 1;
+            }
+        }
+        
+        return ret(len);
+    }
+};
