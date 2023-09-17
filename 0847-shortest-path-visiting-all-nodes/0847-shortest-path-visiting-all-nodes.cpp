5ddbf772a500d31bfe255d6d9249034c05c5fd1c
Rabel Ahmed <rabelhmd@gmail.com>
2023-09-17 07:35:47 +0600

    Time: 10 ms (92.96%), Space: 7.8 MB (96.33%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int dp[12][4096] = {};
+    int dfs(vector<vector<int>>& graph, int& len, int visited, int i, int count) {
+        int res = len * 2;   
+        if (dp[i][visited] > 0) return dp[i][visited] - 1;
+        if ((visited & (1 << i)) == 0) ++count;
+        if (count == len) return 0;        
+        if (dp[i][visited] != -1) {
+            dp[i][visited] = -1;
+            for (auto j : graph[i])
+                res = min(res, 1 + dfs(graph, len, visited | (1 << i), j, count));
+            dp[i][visited] = res + 1;
+        }
+        return res;
+    }
+    
+    int shortestPathLength(vector<vector<int>>& graph, int res = INT_MAX) {
+        int len = graph.size();
+        if (len < 4) return len - 1;
+        for (int i = 0; i < len; i++) res = min(res, dfs(graph, len, 0, i, 0));
+        return res;
+    }
+};
