cec11c6120a284911edc9649f630634046db5d4f
Rabel Ahmed <rabelhmd@gmail.com>
2023-09-28 09:40:08 +0600

    Time: 15 ms (8.62%), Space: 16.7 MB (12.92%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector <int> ret1, ret2;
-        for(auto &x: nums) {
-            (x&1) ? ret2.push_back(x) : ret1.push_back(x);
-        }
-        for(auto &x: ret2) {
-            ret1.push_back(x);
-        }
-        return ret1;
-    } 
+        vector <int> ret;
+        for(auto &x: nums) if(x%2 == 0) ret.push_back(x);
+        for(auto &x: nums) if(x&1) ret.push_back(x);
+        return ret;
+    }
 };
