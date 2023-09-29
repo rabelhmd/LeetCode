2e5f0e818c24c48fe147fc50ca6ce766e520054c
Rabel Ahmed <rabelhmd@gmail.com>
2023-09-29 07:03:51 +0600

    Time: 132 ms (26.24%), Space: 96.8 MB (22.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -8,9 +8,7 @@ public:
             }
             return true;
         }
-        for(int i = 1; i < len; i++) {
-            if(nums[i-1] < nums[i]) return false;
-        }
+        for(int i = 1; i < len; i++) if(nums[i-1] < nums[i]) return false;
         return true;
     }
 };
