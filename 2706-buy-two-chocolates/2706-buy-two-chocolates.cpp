56e67880e2735446715d08b9a7f0c550e0b6c34e
Rabel Ahmed <rabelhmd@gmail.com>
2023-12-20 12:07:52 +1100

    Time: 25 ms (5.34%), Space: 46.9 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int buyChoco(vector<int>& p, int money) {
-        nth_element(begin(p), begin(p) + 2, end(p));
+        sort(p.begin(), p.end());
         int ret = money - (p[0] + p[1]);
         return ret >= 0 ? ret : money;
     }
