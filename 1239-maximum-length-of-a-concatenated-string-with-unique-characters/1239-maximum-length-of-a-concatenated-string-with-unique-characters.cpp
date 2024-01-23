8dc8ab1b294d698367fd834b20662a64267bf425
Rabel Ahmed <rabelhmd@gmail.com>
2024-01-23 22:54:29 +1100

    Time: 176 ms (34.64%), Space: 62.5 MB (35.27%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int dfs(vector<string> &arr, string str = "", int index = 0) {
         unordered_set <char> St(str.begin(), str.end());
-        
-        int ret = str.size();
-        if(St.size() != ret) return 0;
-        for(int i = index; i < arr.size(); i++) {
+        int ret = str.size(), len = arr.size();
+        if(ret != St.size()) return 0;
+        for(int i = index; i < len; i++) 
             ret = max(ret, dfs(arr, str + arr[i], i + 1));
-        }
         return ret;
     }
     
