021d0b6e52d6a9a6cbd492086661e2f262dcfba5
Rabel Ahmed <rabelhmd@gmail.com>
2024-03-09 13:29:38 +1100

    Time: 93 ms (26.31%), Space: 69.1 MB (14.66%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -3,11 +3,7 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         unordered_set <int> St;
         for(auto &x: nums1) St.insert(x);
-        for(auto &x: nums2) if(St.find(x) != St.end()) {
-            //ret.push_back(x);
-            return x;
-        }
-        //if(ret.empty() == false) return ret[0];
+        for(auto &x: nums2) if(St.find(x) != St.end()) return x;
         return -1;
     }
 };
