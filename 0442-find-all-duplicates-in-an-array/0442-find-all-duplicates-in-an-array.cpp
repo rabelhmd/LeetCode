ed1f8e3164ba20a1de7ca84f6c68d0a7f6f47453
Rabel Ahmed <rabelhmd@gmail.com>
2024-03-25 14:42:31 +1100

    Time: 76 ms (21.91%), Space: 48 MB (29.01%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int, int> Mp;
-        for(auto &x: nums) Mp[x] += 1;
-        vector <int> ret;
-        for(auto [k, v]: Mp) if(v == 2) ret.push_back(k);
+        unordered_map <int, int> Mp;
+        vector<int> ret;
+        for(auto &x: nums) {
+            Mp[x] += 1;
+            if(Mp[x] == 2) {
+                ret.push_back(x);
+            }
+        }
         return ret;
     }
 };
