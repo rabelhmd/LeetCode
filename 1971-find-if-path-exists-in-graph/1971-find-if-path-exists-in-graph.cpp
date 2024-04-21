15dc4787f06f662ada122b1ac50e13ae4ddff320
Rabel Ahmed <rabelhmd@gmail.com>
2024-04-21 22:02:07 +1000

    Time: 341 ms (83.14%), Space: 137.7 MB (86.05%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    bool dfs(int cur, int dest, unordered_map<int, vector <int>>&graph,vector<int>&seen) {
-        if(cur == dest) return true;
-        if(!seen[cur]) {
-            seen[cur] = true;
-            for(auto &nextNode: graph[cur]) {
-                if(dfs(nextNode, dest, graph, seen)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+    vector<int> parent;     
+    int findParent(int node) {
+        return parent[node] == node ? node : (parent[node] = findParent(parent[node]));
+    }    
+    void makeSameGroup(int u , int v) {
+        int pu = findParent(u) ; 
+        int pv = findParent(v);
+        parent[pu] = pv; 
     }
     
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map <int,vector<int>> graph;
-        for(auto &e: edges) {
-            int a = e[0], b = e[1];
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-        }
-        vector <int> seen(n);
-        return dfs(source, destination, graph, seen);
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+            parent[i] = i; 
+        
+        for(auto e : edges) {
+            makeSameGroup(e[0] , e[1]); 
+        }                         
+        return findParent(start) == findParent(end); 
     }
 };
