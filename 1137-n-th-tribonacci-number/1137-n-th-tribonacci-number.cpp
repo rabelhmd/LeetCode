e5e7b4ea26a8ecca6077eac931b597cf4ea14b9b
Rabel Ahmed <rabelhmd@gmail.com>
2024-04-24 14:29:07 +1000

    Time: 3 ms (22.77%), Space: 7.4 MB (13.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int tribonacci(int n) {
         if(n == 0) return 0;
-        vector<int> t(n+5, 0);
-        t[1] = 1, t[2] = 1;
-        for(int i = 3; i <= n; i++) t[i] = t[i-1] + t[i-2] + t[i-3];
-        return t[n];
+        vector <int> ret(n+3, 0);
+        ret[1] = ret[2] = 1;
+        for(int i = 3; i <= n; i++) ret[i] = ret[i-1] + ret[i-2] + ret[i-3];
+        return ret[n];
     }
 };
