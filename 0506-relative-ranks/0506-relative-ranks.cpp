62aefd2ba33f141be389eb3510d2ac92132d55a7
Rabel Ahmed <rabelhmd@gmail.com>
2024-05-08 22:55:36 +1000

    Time: 8 ms (66.81%), Space: 19.1 MB (5.21%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -3,18 +3,18 @@ public:
     
 
     vector<string> findRelativeRanks(vector<int>& score) {
-        priority_queue<pair<int,int>> pq;
-        for (int i = 0; i < score.size(); i++) pq.push({score[i], i});
+        vector <int> t = score;
+        sort(t.rbegin(), t.rend());
         
-        int pos = 1;
-        vector <string> ret(score.size());
-        while(pq.empty() == false) {
-            if(pos == 1) ret[pq.top().second] = "Gold Medal";
-            else if(pos == 2) ret[pq.top().second] = "Silver Medal";
-            else if(pos == 3) ret[pq.top().second] = "Bronze Medal";
-            else ret[pq.top().second] = to_string(pos);
-            pq.pop(); pos += 1;
-        }
+        unordered_map <int, string> Mp;
+        Mp[t[0]] = "Gold Medal";
+        if(t.size() >= 2) Mp[t[1]] = "Silver Medal";
+        if(t.size() >= 3) Mp[t[2]] = "Bronze Medal";
+        
+        for(int i = 3; i < t.size(); i++) Mp[t[i]] = to_string(i+1);
+        
+        vector <string> ret;
+        for(auto &x: score) ret.push_back(Mp[x]);
         return ret;
     }
 };
