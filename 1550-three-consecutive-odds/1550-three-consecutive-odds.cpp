beab59daad94a2da33c385c53e7f584fba112544
Rabel Ahmed <rabelhmd@gmail.com>
2024-07-02 03:34:16 +1000

    Time: 3 ms (60.88%), Space: 10.6 MB (8.09%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -3,14 +3,13 @@ public:
     bool isOdd(int x) {
         return x&1;
     }
+    
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int len = arr.size();
-        if(len < 3) return false;
-        for(int i = 0; i < len-2; i++) {
-            int x = arr[i];
-            int y = arr[i+1];
-            int z = arr[i+2];
-            if(isOdd(x) && isOdd(y) && isOdd(z)) return true;
+        int count = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            if(isOdd(arr[i])) count += 1;
+            else count = 0;
+            if(count == 3) return true;
         }
         return false;
     }
