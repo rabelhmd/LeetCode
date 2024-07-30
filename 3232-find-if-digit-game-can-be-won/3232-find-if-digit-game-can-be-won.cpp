053338febf77c11ab9a48281d73b9139549e72a6
Rabel Ahmed <rabelhmd@gmail.com>
2024-07-30 21:30:18 +1000

    Time: 14 ms (6.67%), Space: 28 MB (83.76%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -5,11 +5,7 @@ public:
         for(auto &x: nums) {
             if(x < 10) a += x;
             else b += x;
-            if(x >= 10) c += x;
-            else d += x;
         }
-        if(a > b and (a or b)) return true;
-        if(c > d and (c or d)) return true;
-        return false;
+        return a != b;
     }
 };
