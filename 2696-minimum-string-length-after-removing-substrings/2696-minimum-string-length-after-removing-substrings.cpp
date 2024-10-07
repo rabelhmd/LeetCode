b50706e5b72fbdc996c5ba47e8ced739fc3278b0
Rabel Ahmed <rabelhmd@gmail.com>
2024-10-07 15:14:01 +1100

    Time: 4 ms (76.08%), Space: 11.4 MB (76.70%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -8,14 +8,12 @@ public:
     int minLength(string s) {
         faster();
         string ret;
-        int id = 0;
         for(auto &c: s) {
-            ret += c; id += 1;
-            if(id > 1) {
-                if((ret[id-2] == 'A' and ret[id-1] == 'B') or (ret[id-2] == 'C' and ret[id-1] == 'D')) {
-                    ret.pop_back(), ret.pop_back();
-                    id -= 2;
-                }       
+            ret += c;
+            int len = ret.size();
+            if(len >= 2) {
+                string sub = ret.substr(len-2, 2);
+                if(sub == "AB" or sub == "CD") ret.pop_back(), ret.pop_back();
             }
         }
         return ret.size();
